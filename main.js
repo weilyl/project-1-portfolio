8ab7f76f0b8c7f7cc7b9464cd3cf5ef977eba220
weilyl <weily.lang@gmail.com>
2020-07-16 13:10:41 -0400

    linked Projects Google Sheet with HTML using JSON API

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
+// Hamburger Menu
 $(document).ready(function() {
     $('.hamburger-icon').on('click', function() {
         $('nav').toggle(500)
     })
 });
 
+
+// Projects JSON
+const projURL = 'https://spreadsheets.google.com/feeds/list/1S5GibHmW7yaGIyCBTNvm6omER6XzQ-4iiCdtDMrCJtg/od6/public/values?alt=json';
+fetch(projURL)
+    .then(response => response.json())
+    .then(data => {
+        // console.log(`data:`, data)
+        console.log(data.feed.entry)
+        const projects = data.feed.entry.map( entry => {
+            return {
+                title: entry.gsx$title.$t,
+                image: entry.gsx$image.$t,
+                description: entry.gsx$description.$t,
+                url: entry.gsx$url.$t
+            }
+        })
+        app(projects)
+    })
+ 
+const app = (data) => {
+    console.log('app is running!')
+    console.log(data)
+
+    const createProjectElement = (project) => {
+        const $div = $('<div>');
+        $div.append($('<h2>').text(project.title));
+        $div.append($('<p>').text(project.description));
+        $div.append($('<img>').attr('src', project.image));
+        $div.append($('<a>').attr('href', project.url).text('LINK >>>'));
+        return $div;
+    
+    }
+    // $('body').append(createProjectElement(data[0]))
+    
+    data.forEach( project => {
+        const $projectDiv = createProjectElement(project);
+        $('body').append($projectDiv);
+    })
+}
+
+
+// Contact Form
 function getInput() {
     // obtain user input from contact form
     const $name = $('#name');
@@ -12,14 +55,15 @@ function getInput() {
     const $message = $('#msg');
 
     // Google Form URL
-    const $formURL = 'https://docs.google.com/forms/u/1/d/e/1FAIpQLScUP8hSnb0-SKNnOLZlwhUlhx89k85n_FgXEVBdJdT2N2fNIA/formResponse?'; 
+    const $formURL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeZSjsZbKua9ukZPDot0VyTd43JFPoyFLdqBbngJPx1Id5LAA/formResponse'; 
     
     // may or may not need ?
 
     // tags for input from Google Form URL
-    const $googleName ="entry.2005620554";
-    const $googleEmail = "entry.1045781291";
-    const $googleMessage = "entry.839337160";
+    const $googleName ="entry.1389903601";
+    const $googleEmail = "entry.1487871588";
+    const $googleSubject = "entry.1386304243";
+    const $googleMessage = "entry.entry.39027611";
 
     // TEST
     console.log($message.val());
@@ -27,7 +71,7 @@ function getInput() {
 
     // sent content via POST
     // Kenya's way: <form action="https://docs.google.com/forms/.../formResponse" method="POST" id=""> <input name="entry....">
-    console.log(`${$googleName}=${$name.val()}&${$googleEmail}=${$email.val()}&${$googleMessage}=${$message.val()}`);
+    console.log(`${$googleName}=${$name.val()}&${$googleEmail}=${$email.val()}&${$googleSubject}=${$subject.val()}&${$googleMessage}=${$message.val()}`);
 
     fetch($formURL, {
         method: 'POST',
@@ -35,11 +79,12 @@ function getInput() {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: `${$googleName}=${$name.val()}&${$googleEmail}=${$email.val()}&${$googleMessage}=${$message.val()}`
+        body: `${$googleName}=${$name.val()}&${$googleEmail}=${$email.val()}&${$googleSubject}=${$subject.val()}&${$googleMessage}=${$message.val()}`
     })
     .then(res => {
         $name.val('');
         $email.val('');
+        $subject.val('');
         $message.val('');
     })
     .catch(err => console.log(err));
