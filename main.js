3fd18cd3070f79089c6704cbcdb21825dbad073e
weilyl <weily.lang@gmail.com>
2020-07-16 22:51:53 -0400

    Added function and styling for carousel buttons

--- main.js
+++ main.js
@@ -2,12 +2,39 @@
 $(document).ready(function() {
     $('.hamburger-icon').on('click', function() {
         $('nav').toggle(500)
-    })
+    });
+});
+
+// Hide carousel buttons if there are no projects
+$(document).ready(function() {
+    if ($('#projects-container').children().length = 0) {
+        $('#carousel-container').css('display', 'none');
+    }; 
 });
 
 // Previous & Next buttons
-$('#previous').on('click', () => {
-    
+$(() => {
+    let currentImgIndex = 0;
+    let highestImgIndex = $('#projects-container').children().length - 1;
+    $('#next').on('click', () => {
+        $('#projects-container').children().eq(currentImgIndex).css('display','none');
+        if (currentImgIndex < highestImgIndex) {
+            currentImgIndex++;
+        } else { 
+            currentImgIndex = 0;
+        };
+        $('#projects-container').children().eq(currentImgIndex).css('display','block');
+    });
+    $('#previous').on('click', () => {
+        $('#projects-container').children().eq(currentImgIndex).css('display','none');
+        if (currentImgIndex > 0) {
+            currentImgIndex--;
+        } else {
+            currentImgIndex = highestImgIndex;
+        };
+        $('#projects-container').children().eq(currentImgIndex).css('display','block');
+    });
+});
 
 
 // Projects JSON (Create array of objects for projects)
@@ -17,7 +44,7 @@ fetch(projURL)
     .then(data => {
         // console.log(`data:`, data)
         console.log(data.feed.entry)
-        const projects = data.feed.entry.map( entry => {
+        const projects = data.feed.entry.map(entry => {
             return {
                 title: entry.gsx$title.$t,
                 image: entry.gsx$image.$t,
@@ -30,7 +57,7 @@ fetch(projURL)
 
 // Function to look through projects array and add to HTML
 const createProjectElements = (projects) => {    
-    for (i=0; i<projects.length; i++) {
+    for (i = 0; i < projects.length; i++) {
         // Create one div per project
         const $projElement = $('<div>').attr('class', 'project-element');
         const $img = $('<img>').attr('src', projects[i].image);
@@ -57,15 +84,17 @@ function getInput() {
     const $message = $('#msg');
 
     // Google Form URL
-    const $formURL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeZSjsZbKua9ukZPDot0VyTd43JFPoyFLdqBbngJPx1Id5LAA/formResponse'; 
+    const $formURL = 'https://docs.google.com/forms/u/1/d/e/1FAIpQLScUP8hSnb0-SKNnOLZlwhUlhx89k85n_FgXEVBdJdT2N2fNIA/formResponse';
+    
+    // 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeZSjsZbKua9ukZPDot0VyTd43JFPoyFLdqBbngJPx1Id5LAA/formResponse'; 
     
     // may or may not need ?
 
     // tags for input from Google Form URL
-    const $googleName ="entry.1389903601";
-    const $googleEmail = "entry.1487871588";
-    const $googleSubject = "entry.1386304243";
-    const $googleMessage = "entry.entry.39027611";
+    const $googleName ='entry.2005620554'; //"entry.1389903601";
+    const $googleEmail = 'entry.1160907507'; //"entry.1487871588";
+    const $googleSubject = 'entry.1045781291'; //"entry.1386304243";
+    const $googleMessage = 'entry.839337160'; //"entry.39027611";
 
     // TEST
     console.log($message.val());
